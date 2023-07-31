6bcc9847ef4f726eaa18019113c44dbfc38d5af0
blktheta <blktheta@gmail.com>
2023-07-31 17:49:35 +0200

    Add API endpoint to Book model

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd party
+    "rest_framework",
     # Local
     "books.apps.BooksConfig",
+    "apis.apps.ApisConfig",
 ]
 
 MIDDLEWARE = [
